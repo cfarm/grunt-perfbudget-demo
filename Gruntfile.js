e84ad514608370dacb8fe06314fa2f6febfa128a
Catherine Farman <cfarman@gmail.com>
2014-11-07 14:57:25 -0500

    install perfbudget plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,64 +4,15 @@ module.exports = function(grunt){
 	grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    sass: {
-      options: {
-        precision: 5
-      },
-      dev: {
+    perfbudget: {
+      default: {
         options: {
-          style: 'nested', // compact, compressed, nested or expanded
-          sourcemap: true
-        },
-        files: {
-          'stylesheets/css/styles.css' : 'stylesheets/scss/styles.scss'
-        }
-      },
-      prod: {
-        options: {
-          style: 'compressed', // compact, compressed, nested or expanded
-          sourcemap: true
-        },
-        files: {
-          'stylesheets/css/styles.css' : 'stylesheets/scss/styles.scss'
+          url: 'http://cfarman.com',
+          key: 'APIKEY'
         }
       }
-    },
+    }
+  });
 
-    uglify: {
-      dev: {
-        options: {
-          beautify: true
-        },
-        files: {
-          // Where to combine and minify JS files, followed by list of which files to include and exclude
-          'js/script.min.js' : ['js/scripts/*.js', 'js/script.js', '!js/plugins/modernizr.js']
-        }
-      },
-      prod: {
-        files: {
-          // Where to combine and minify JS files, followed by list of which files to include and exclude
-          'js/script.min.js' : ['js/plugins/*.js', 'js/script.js', '!js/plugins/modernizr.js', '!js/plugins/livereload.js']
-        }
-      }
-    },
-
-    // Watch options: what tasks to run when changes to files are saved
-    watch: {
-      options: {
-        livereload: true
-      },
-      css: {
-        files: ['stylesheets/scss/*.scss'],
-        tasks: ['sass:dev']
-      },
-      js: {
-        files: ['js/scripts/*.js', '!js/script.min.js'], // Watch for changes in JS files except for script.min.js to avoid reload loops
-        tasks: ['uglify:dev']
-      }
-		}
-	});
-
-  grunt.registerTask('default', ['sass:dev','uglify:dev','watch']);
-  grunt.registerTask('production', ['sass:prod','uglify:prod']);
+  grunt.registerTask('default', ['perfbudget']);
 };
